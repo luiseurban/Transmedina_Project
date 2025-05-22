75c48458415f244c8cd39bd7bfa74c0c44321722
Samuel Ospina <samiospina2006@gmail.com>
2025-05-21 19:56:37 -0500

    Añadiendo filtros para novedades

--- conductores/views.py
+++ conductores/views.py
@@ -238,9 +238,55 @@ def delete_mototaxi(request, placa_mototaxi):
 #CRUD NOVEDADES
 @login_required
 def novedades_main_view(request):
-    is_admin = request.user.is_staff
+      #Obtener párametros del GET
+    tipo_novedad = request.GET.get('tipo_novedad', "todos")
+    conductor_id = request.GET.get('conductor', "todos")
+    ordenar_tipo = request.GET.get('ordenar_tipo', "")
+    ordenar_conductor = request.GET.get('ordenar_conductor', "")
+    
     novedades = Novedades.objects.all()
-    return render(request, 'pages/NovedadesPages/novedades_main_view/novedades_main_view.html', {'is_admin': is_admin, 'novedades': novedades})
+
+    #Filtro por tipo de novedad
+    if tipo_novedad and tipo_novedad != "todos":
+        novedades = novedades.filter(tipo_novedad=tipo_novedad)
+
+    #Filtro por conductor
+    if conductor_id and conductor_id != "todos":
+        try:
+          conductor_id = int(conductor_id)
+          novedades = novedades.filter(conductor_id=int(conductor_id))    
+        except ValueError:
+            pass #Ignora si no es un número
+
+    #Ordenar por tipo de novedad
+    if ordenar_tipo == "reciente":
+        novedades = novedades.order_by('-id')
+    elif ordenar_tipo == "antiguo":
+        novedades = novedades.order_by('id')
+    
+    #Ordenar por conductor
+    if ordenar_conductor == "asc":
+        novedades = novedades.order_by('conductor__nombre') 
+    elif ordenar_conductor == "desc":
+        novedades = novedades.order_by('-conductor__nombre')
+    elif ordenar_conductor == "recientes":
+        novedades = novedades.order_by('-conductor__fecha_de_creacion')
+    
+    #Obtener la lista única de tipos de novedad para el filtro
+    tipos_novedad = Novedades.ESTADOS
+
+    
+
+    return render(request, 'pages/NovedadesPages/novedades_main_view/novedades_main_view.html', {
+        'novedades': novedades,
+        'tipos_novedad': tipos_novedad,
+        'conductores': Conductores.objects.all(),
+        'tipo_novedad_seleccionado': tipo_novedad,
+        'conductor_seleccionado': conductor_id,
+        'ordenar_tipo': ordenar_tipo,
+        'ordenar_conductor': ordenar_conductor,
+        'is_admin': True,
+    })
 
 @login_required
 def create_novedad(request):
